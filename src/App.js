4b2dfa6da7ba98f393acd090d2910527d03fc352
AnanSer <ananserbesa@gmail.com>
2024-10-18 23:17:19 +0300

    restructuring the addition of Meals components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Fragment } from "react";
 
 import Header from "./Components/Layout/Header";
-
-import MealsSummary from "./Components/Meals/MealsSummary";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
